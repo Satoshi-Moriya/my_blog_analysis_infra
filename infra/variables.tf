4fc0caa61a66b1cf44be103282d6e1ab438d4f06
Satoshi-Moriya <moriyasatoshi1994@gmail.com>
2025-09-09 07:47:53 +0900

    saを追加した（データセットは一旦削除)

---
+++ infra/variables.tf
@@ -0,0 +1,16 @@
+variable "project_id" {
+  description = "Google CloudのProject ID"
+  type        = string
+}
+
+variable "bq_location" {
+  description = "BigQueryのlocation"
+  type        = string
+  default     = "asia-northeast1"
+}
+
+variable "region" {
+  description = "Google Cloudのregion"
+  type        = string
+  default     = "asia-northeast1"
+}
