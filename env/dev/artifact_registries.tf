b140210429f67f4f88cbff1bd126eb60b20d312a
Satoshi Moriya <moriyasatoshi1994@gmail.com>
2026-05-13 22:29:54 +0900

    refactor: modules化した

--- env/dev/artifact_registries.tf
+++ env/dev/artifact_registries.tf
@@ -1,6 +1,5 @@
-resource "google_artifact_registry_repository" "my_repo" {
-  repository_id = "my-repository"
-  location      = var.location
-  description   = "docker repository"
-  format        = "DOCKER"
+module "artifact_registry" {
+  source = "../../modules/ar"
+
+  location = var.location
 }
