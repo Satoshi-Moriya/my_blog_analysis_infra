2e0dd3d1d16b17be6726d144fbca78848ced032b
Satoshi Moriya <moriyasatoshi1994@gmail.com>
2026-05-21 23:39:19 +0900

    refactor: modules化した

--- env/prod/cloud_run_jobs.tf
+++ env/prod/cloud_run_jobs.tf
@@ -1,35 +1,10 @@
-resource "google_cloud_run_v2_job" "dbt_job" {
-  name                = "my-blog-analysis-dbt-job"
-  location            = var.location
-  deletion_protection = false
+module "dbt_job" {
+  source = "../../modules/cloud_run_jobs"
 
-  template {
-    template {
-      containers {
-        image   = "asia-northeast1-docker.pkg.dev/${var.project_id}/my-repository/my-blog-analysis-etl:latest"
-        command = ["bash"]
-        args    = ["/app/scripts/batch.sh"]
+  location          = var.location
+  project_id        = var.project_id
+  ga_raw_dataset_id = var.ga_raw_dataset_id
+  env               = var.env
 
-        env {
-          name  = "DBT_TARGET"
-          value = "prod"
-        }
-
-        env {
-          name  = "DBT_GCP_PROJECT"
-          value = var.project_id
-        }
-
-        env {
-          name  = "DBT_BIGQUERY_DATASET"
-          value = var.ga_raw_dataset_id
-        }
-
-      }
-      service_account = module.service_accounts.sa_dbt_runner_email
-
-      max_retries = 1
-      timeout     = "3600s"
-    }
-  }
+  service_accounts_email = module.service_accounts.sa_dbt_runner_email
 }
