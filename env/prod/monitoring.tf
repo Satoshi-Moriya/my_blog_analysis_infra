86755ec0ab705447f28743646a711f69cb90d1bd
Satoshi Moriya <moriyasatoshi1994@gmail.com>
2026-06-08 23:31:10 +0900

    fix: フォーマットを修正

--- env/prod/monitoring.tf
+++ env/prod/monitoring.tf
@@ -1,9 +1,9 @@
 module "monitoring" {
   source = "../../modules/monitoring"
 
-  location            = var.location
+  location           = var.location
   project_id         = var.project_id
   notification_email = var.notification_email
 
-  dbt_job_name       = module.dbt_job.dbt_job_name
+  dbt_job_name = module.dbt_job.dbt_job_name
 }
