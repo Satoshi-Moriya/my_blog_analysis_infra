613cca308c8f84b1ef08f6d92432faaae272250a
Satoshi Moriya <moriyasatoshi1994@gmail.com>
2026-06-16 23:23:53 +0900

    feat: prd環境の新しいデータセットへの実行権限を追加

--- env/prod/iam.tf
+++ env/prod/iam.tf
@@ -37,3 +37,31 @@ resource "google_bigquery_dataset_iam_member" "raw_viewer" {
   role       = "roles/bigquery.dataViewer"
   member     = "serviceAccount:${module.service_accounts.sa_dbt_runner_email}"
 }
+
+resource "google_bigquery_dataset_iam_member" "new_staging_editor" {
+  dataset_id = module.datasets.staging_dataset_id
+  project    = var.project_id
+  role       = "roles/bigquery.dataEditor"
+  member     = "serviceAccount:${module.service_accounts.sa_dbt_runner_email}"
+}
+
+resource "google_bigquery_dataset_iam_member" "new_intermediate_editor" {
+  dataset_id = module.datasets.intermediate_dataset_id
+  project    = var.project_id
+  role       = "roles/bigquery.dataEditor"
+  member     = "serviceAccount:${module.service_accounts.sa_dbt_runner_email}"
+}
+
+resource "google_bigquery_dataset_iam_member" "new_mart_editor" {
+  dataset_id = module.datasets.mart_dataset_id
+  project    = var.project_id
+  role       = "roles/bigquery.dataEditor"
+  member     = "serviceAccount:${module.service_accounts.sa_dbt_runner_email}"
+}
+
+resource "google_bigquery_dataset_iam_member" "new_raw_viewer" {
+  dataset_id = var.ga_raw_dataset_id
+  project    = var.project_id
+  role       = "roles/bigquery.dataViewer"
+  member     = "serviceAccount:${module.service_accounts.sa_dbt_runner_email}"
+}
