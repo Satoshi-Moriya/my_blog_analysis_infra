7342e9cea50cad9cd4aff872cecad3edcf6ae94f
Satoshi Moriya <moriyasatoshi1994@gmail.com>
2026-06-26 23:58:33 +0900

    fix: リソース名をdevに合わせた

--- env/prod/iam.tf
+++ env/prod/iam.tf
@@ -10,35 +10,28 @@ resource "google_project_iam_member" "scheduler_invoker" {
   member  = "serviceAccount:${module.service_accounts.sa_dbt_runner_email}"
 }
 
-resource "google_bigquery_dataset_iam_member" "raw_viewer" {
-  dataset_id = var.ga_raw_dataset_id
-  project    = var.project_id
-  role       = "roles/bigquery.dataViewer"
-  member     = "serviceAccount:${module.service_accounts.sa_dbt_runner_email}"
-}
-
-resource "google_bigquery_dataset_iam_member" "new_staging_editor" {
+resource "google_bigquery_dataset_iam_member" "staging_editor" {
   dataset_id = module.datasets.staging_dataset_id
   project    = var.project_id
   role       = "roles/bigquery.dataEditor"
   member     = "serviceAccount:${module.service_accounts.sa_dbt_runner_email}"
 }
 
-resource "google_bigquery_dataset_iam_member" "new_intermediate_editor" {
+resource "google_bigquery_dataset_iam_member" "intermediate_editor" {
   dataset_id = module.datasets.intermediate_dataset_id
   project    = var.project_id
   role       = "roles/bigquery.dataEditor"
   member     = "serviceAccount:${module.service_accounts.sa_dbt_runner_email}"
 }
 
-resource "google_bigquery_dataset_iam_member" "new_mart_editor" {
+resource "google_bigquery_dataset_iam_member" "mart_editor" {
   dataset_id = module.datasets.mart_dataset_id
   project    = var.project_id
   role       = "roles/bigquery.dataEditor"
   member     = "serviceAccount:${module.service_accounts.sa_dbt_runner_email}"
 }
 
-resource "google_bigquery_dataset_iam_member" "new_raw_viewer" {
+resource "google_bigquery_dataset_iam_member" "raw_viewer" {
   dataset_id = var.ga_raw_dataset_id
   project    = var.project_id
   role       = "roles/bigquery.dataViewer"
