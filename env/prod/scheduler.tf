93311a7adcf48aa14d36dead8738b08213d9a0b2
Satoshi Moriya <moriyasatoshi1994@gmail.com>
2026-07-04 23:29:12 +0900

    refactor: リソースのアドレスが変更しないようにつけていたmovedを削除

--- env/prod/scheduler.tf
+++ env/prod/scheduler.tf
@@ -6,8 +6,3 @@ module "scheduler" {
   service_accounts_email = module.service_accounts.sa_dbt_runner_email
   dbt_job_name           = module.dbt_job.dbt_job_name
 }
-
-moved {
-  from = google_cloud_scheduler_job.dbt_job_scheduler
-  to   = module.scheduler.google_cloud_scheduler_job.dbt_job_scheduler[0]
-}
